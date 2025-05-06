bcbac64b7e35393cf6bbef709072ed108faebdb0
CynthiaHe0 <67443885+CynthiaHe0@users.noreply.github.com>
2025-05-06 03:17:21 -0700

    Fixed Arcane Blast secondary projectiles

--- Assets/Scripts/Spells/Base Spells/ArcaneBlast.cs
+++ Assets/Scripts/Spells/Base Spells/ArcaneBlast.cs
@@ -80,7 +80,7 @@ public class ArcaneBlast : Spell
         }
         float degree_gap = 360f / n;
         for (int i = 0; i < (int) n; i++) {
-            Vector3 direction = vector + new Vector3(Mathf.Sin(degree_gap * i * Mathf.PI/180), Mathf.Cos(degree_gap * i * Mathf.PI / 180), 0);
+            Vector3 direction = new Vector3(Mathf.Sin(degree_gap * i), Mathf.Cos(degree_gap * i), 0);
             GameManager.Instance.projectileManager.CreateProjectile(
                 secondary_projectile_icon, secondary_projectile_path, 
                 vector, direction, secondary_projectile_speed, OnSecondHit, GetLifeTime());
