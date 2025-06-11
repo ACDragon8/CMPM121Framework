8bb8c8763f8a1801af5232eee071cb8787230017
ACDragon <haodeng8@gmail.com>
2025-06-10 18:26:38 -0700

    toggle for craft menu

--- Assets/Scripts/Crafting/CraftingManager.cs
+++ Assets/Scripts/Crafting/CraftingManager.cs
@@ -6,17 +6,30 @@ public class CraftingManager : MonoBehaviour
 {
 
     public GameObject craftUI;
+    public int materials;
+    private bool toggle;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        toggle = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    public void ToggleCraftMenu()
+    {
+        if (toggle) {
+            toggle = false;
+        }
+        else {
+            toggle = true;
+        }
+        craftUI.SetActive(toggle);
     }
 
     public void OpenCraftMenu()
@@ -28,6 +41,11 @@ public class CraftingManager : MonoBehaviour
     public void CloseCraftMenu()
     {
         craftUI.SetActive(false);
+    }
+
+    public bool checkCraftable()
+    {
+        return false;
     }
 
 
